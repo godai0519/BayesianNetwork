42099da3a4df5deefef26f9713b0605ef8b856d1
Godai Azuma <godai0519@gmail.com>
2015-01-18 23:17:27 +0900

    update: in/out_vertexs are implemented.(a.k.a in/out_edges)
    Signed-off-by: Godai Azuma <godai0519@gmail.com>

--- bayesian/graph.hpp
+++ bayesian/graph.hpp
@@ -133,6 +133,17 @@ public:
         return ret;
     }
 
+    // 引数の頂点から出て行く隣接頂点を列挙する
+    std::vector<vertex_type> out_vertexs(vertex_type const& from) const
+    {
+        std::vector<vertex_type> ret;
+        for(auto const& edge : out_edges(from))
+        {
+            ret.push_back(target(edge));
+        }
+        return ret;
+    }
+
     // 引数の頂点へ入っていく辺を列挙する
     std::vector<edge_type> in_edges(vertex_type const& to) const
     {
@@ -145,6 +156,17 @@ public:
             {
                 ret.push_back(adjacent_list_[i][index]);
             }
+        }
+        return ret;
+    }
+
+    // 引数の頂点へ入っていく隣接頂点を列挙する
+    std::vector<vertex_type> in_vertexs(vertex_type const& to) const
+    {
+        std::vector<vertex_type> ret;
+        for(auto const& edge : in_edges(to))
+        {
+            ret.push_back(source(edge));
         }
         return ret;
     }
