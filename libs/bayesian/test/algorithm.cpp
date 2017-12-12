a9593d25826dd4710e815b993847a72de9cb32c8
Godai Azuma <godai0519@gmail.com>
2017-12-13 00:57:13 +0900

    Add a test program of topological_sort.
    Signed-off-by: Godai Azuma <godai0519@gmail.com>

---
+++ libs/bayesian/test/algorithm.cpp
@@ -0,0 +1,58 @@
+#define BOOST_TEST_MAIN
+#include <boost/test/included/unit_test.hpp>
+#include <boost/test/data/test_case.hpp>
+#include <boost/test/data/monomorphic.hpp>
+#include <bayesian/network.hpp>
+#include <bayesian/network/adjacency_list.hpp>
+#include <bayesian/network/adjacency_matrix.hpp>
+#include <bayesian/algorithm.hpp>
+
+BOOST_AUTO_TEST_CASE(topological_sort_1)
+{
+    // v1 -> v2 -> v3 -> v4 -> v5
+    bn::network<bn::adjacency_list> network;
+    for(int i = 0; i < 5; ++i) network.add_node();
+    for(int i = 0; i < 4; ++i)
+        network.add_arc(
+            network.all_node().at(i),
+            network.all_node().at(i + 1));
+
+    auto const topological = bn::topological_sort(network);
+    BOOST_CHECK(
+        std::equal(
+            topological.crbegin(), topological.crend(),
+            network.all_node().cbegin(), network.all_node().cend()));
+
+    BOOST_CHECK(bn::is_dag(network) == true);
+}
+
+BOOST_AUTO_TEST_CASE(topological_sort_2)
+{
+	bn::network<bn::adjacency_list> network;
+    for(int i = 0; i < 6; ++i) network.add_node();
+    network.add_arc(network.all_node().at(0), network.all_node().at(1));
+    network.add_arc(network.all_node().at(0), network.all_node().at(2));
+    network.add_arc(network.all_node().at(1), network.all_node().at(3));
+    network.add_arc(network.all_node().at(2), network.all_node().at(3));
+    network.add_arc(network.all_node().at(3), network.all_node().at(4));
+    network.add_arc(network.all_node().at(3), network.all_node().at(5));
+
+    auto const topological = bn::topological_sort(network);
+    BOOST_CHECK(bn::is_dag(network) == true);
+}
+
+BOOST_AUTO_TEST_CASE(topological_sort_3)
+{
+	bn::network<bn::adjacency_list> network;
+    for(int i = 0; i < 6; ++i) network.add_node();
+    network.add_arc(network.all_node().at(0), network.all_node().at(2));
+    network.add_arc(network.all_node().at(1), network.all_node().at(0));
+    network.add_arc(network.all_node().at(2), network.all_node().at(3));
+    network.add_arc(network.all_node().at(3), network.all_node().at(1));
+    network.add_arc(network.all_node().at(3), network.all_node().at(4));
+    network.add_arc(network.all_node().at(3), network.all_node().at(5));
+
+    auto const topological = bn::topological_sort(network);
+    BOOST_CHECK(bn::is_dag(network) == false);
+}
+
